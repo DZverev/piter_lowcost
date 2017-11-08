2695768591833e8769aa2b71c36f4613a72c9cc2
Aleksei_Okatiev <aleksei_okatiev@epam.com>
2017-11-08 15:25:24 +0300

    added ant design

--- app/javascript/app/src/containers/App.js
+++ app/javascript/app/src/containers/App.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
 
+import { LocaleProvider } from 'antd';
+import ruRU from 'antd/lib/locale-provider/ru_RU';
+
 class App extends Component {
   render() {
     return (
-      <div>
-        App
-      </div>
+      <LocaleProvider locale={ruRU}>
+        <div>
+          App
+        </div>
+      </LocaleProvider>
     );
   }
 }
--- config/webpack/loaders/sass.js
+++ config/webpack/loaders/sass.js
@@ -2,14 +2,15 @@ const ExtractTextPlugin = require('extract-text-webpack-plugin')
 const { env } = require('../configuration.js')
 
 module.exports = {
-  test: /\.(scss|sass|css)$/i,
+  test: /\.(scss|sass|less|css)$/i,
   use: ExtractTextPlugin.extract({
     fallback: 'style-loader',
     use: [
       { loader: 'css-loader', options: { minimize: env.NODE_ENV === 'production' } },
       { loader: 'postcss-loader', options: { sourceMap: true } },
       'resolve-url-loader',
-      { loader: 'sass-loader', options: { sourceMap: true } }
+      { loader: 'sass-loader', options: { sourceMap: true } },
+      { loader: 'less-loader', options: { sourceMap: true } }
     ]
   })
 }
