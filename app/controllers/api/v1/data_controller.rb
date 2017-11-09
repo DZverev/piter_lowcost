a308d57fb3e5b0cd90bb833fb0c868ffef4b1efc
Aleksei_Okatiev <aleksei_okatiev@epam.com>
2017-11-09 10:42:44 +0300

    rubocop fixes

--- app/controllers/api/v1/data_controller.rb
+++ app/controllers/api/v1/data_controller.rb
@@ -11,11 +11,10 @@ module Api
         data = []
         20.times do
           data << { id: Faker::Number.unique.number(3),
-            to: Faker::Space.moon,
-            from: Faker::Space.galaxy,
-            date: Faker::Date.between(Date.today, 1.year.from_now),
-            price: Faker::Number.decimal(2)
-          }
+                    to: Faker::Space.moon,
+                    from: Faker::Space.galaxy,
+                    date: Faker::Date.between(Time.zone.today, 1.year.from_now),
+                    price: Faker::Number.decimal(2) }
         end
         data
       end
